6ddc70bc97da1d346536690936790c6612b47c6b
Ajith Thampi <josephajithampi@gmail.com>
2025-01-20 17:05:23 +0000

    first commit

---
+++ parks-plugin.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ * Parks Plugin
+ *
+ * @package           Parks
+ * @author            Ajith Thampi Joseph
+ * @copyright         2025 Ajith Thampi Joseph
+ * @license           GPL-2.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Parks Plugin
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Your Name
+ * Author URI:        https://example.com
+ * Text Domain:       plugin-slug
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ * Requires Plugins:  my-plugin, yet-another-plugin
+ */
